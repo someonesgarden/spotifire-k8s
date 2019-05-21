cc4206593cad1085c59631e2c2ebe4961cf4db39
Daisuke Nishimura <d@someonesgarden.org>
2019-05-22 01:18:22 +0900

    added deployment scripts2

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+docker build -t someonesgarden/spotifire-client:latest -t someonesgarden/spotifire-client:$SHA -f ./client/Dockerfile ./client
+docker build -t someonesgarden/spotifire-api:latest -t someonesgarden/spotifire-api:$SHA -f ./api/Dockerfile ./api
+docker push someonesgarden/spotifire-client:latest
+docker push someonesgarden/spotifire-api:latest
+docker push someonesgarden/spotifire-client:$SHA
+docker push someonesgarden/spotifire-api:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/api-deployment api=someonesgarden/spotifire-api:$SHA
+kubectl set image deployments/client-deployment client=someonesgarden/spotifire-client:$SHA
