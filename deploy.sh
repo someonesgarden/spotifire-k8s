7873ae0889ace2089535cb84080e5eb2c0ed8064
Daisuke Nishimura <d@someonesgarden.org>
2019-05-22 21:22:16 +0900

    rollback

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-#kubectl delete -f k8s
-docker build -t someonesgarden/spotifire-client:latest -t someonesgarden/spotifire-client:$SHA -f ./client/Dockerfile ./client
-docker build -t someonesgarden/spotifire-api:latest -t someonesgarden/spotifire-api:$SHA -f ./api/Dockerfile ./api
-docker push someonesgarden/spotifire-client:latest
-docker push someonesgarden/spotifire-api:latest
-docker push someonesgarden/spotifire-client:$SHA
-docker push someonesgarden/spotifire-api:$SHA
-
-kubectl apply -f k8s
-kubectl set image deployments/api-deployment api=someonesgarden/spotifire-api:$SHA
-kubectl set image deployments/client-deployment client=someonesgarden/spotifire-client:$SHA
+kubectl delete -f k8s
+#docker build -t someonesgarden/spotifire-client:latest -t someonesgarden/spotifire-client:$SHA -f ./client/Dockerfile ./client
+#docker build -t someonesgarden/spotifire-api:latest -t someonesgarden/spotifire-api:$SHA -f ./api/Dockerfile ./api
+#docker push someonesgarden/spotifire-client:latest
+#docker push someonesgarden/spotifire-api:latest
+#docker push someonesgarden/spotifire-client:$SHA
+#docker push someonesgarden/spotifire-api:$SHA
+#
+#kubectl apply -f k8s
+#kubectl set image deployments/api-deployment api=someonesgarden/spotifire-api:$SHA
+#kubectl set image deployments/client-deployment client=someonesgarden/spotifire-client:$SHA
