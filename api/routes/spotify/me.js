674b676337b012fe392da6edf83f24df97aa41bd
Daisuke Nishimura <d@someonesgarden.org>
2019-05-27 13:51:29 +0900

    me changed

--- api/routes/spotify/me.js
+++ api/routes/spotify/me.js
@@ -20,6 +20,28 @@ router.get('/get_me/:access_token', (req,res)=>{
 });
 
 
+router.get('/get_my_toptracks/:access_token', (req,res)=>{
+
+    console.log("get my top tracks calling..");
+
+    let access_token = req.params.access_token;
+
+    console.log(access_token);
+    spotifyApi.setAccessToken(access_token);
+    spotifyApi.getMyTopTracks().then(
+        function(data) {
+            console.log("get my top tracks success!");
+            res.send({...data.body});
+        },
+        function(err) {
+            console.log('Could not refresh the token!', err.message);
+            res.send(null);
+        }
+    );
+});
+
+
+
 router.get('/get_user_playlists/:access_token/:userid', (req,res)=>{
     let access_token = req.params.access_token;
     let user_id = req.params.userid;
