28f803858dd74137ae681ac76c1a104bc4331647
Daisuke Nishimura <d@someonesgarden.org>
2019-06-13 11:27:44 +0900

    deviceorientation event

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -4,109 +4,23 @@
 
             <mu-flex class="flex-wrapper" justify-content="center" align-items="center" direction="column">
 
-                <h1>deviceorientation</h1>
+                <h3 style="font-weight:bold;color:#0de254;">deviceorientation</h3>
                 <hr>
 
-                <mu-form ref="sensorform" :model="sensor" class="mu-demo-form" label-position="top" label-width="50">
+                <div class="ui grid">
 
-                    <div class="ui grid">
-                        <div class="four wide column">
-                            <mu-form-item prop="angle" label="Angle" class="range">
-                                <mu-text-field prop="angle" v-model="sensor.angleH"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="x" label="x" class="range">
-                                <mu-text-field prop="x"  v-model="sensor.hx"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="y" label="x" class="range">
-                                <mu-text-field prop="y"  v-model="sensor.hy"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="z" label="x" class="range">
-                                <mu-text-field prop="z"  v-model="sensor.hz"></mu-text-field>
-                            </mu-form-item>
-                        </div>
+                    <div class="eight wide column" style="text-align:center;">
+                        <img id="compass_h" ref="compass_h" src="/static/img/spotify_logo.png" style="width:100px;height:100px;border-radius:50%;"/><br/>
+                        Angle(h):{{sensor.angleH}}<br/>
+                        (x,y,z):({{sensor.hx}},{{sensor.hy}},{{sensor.hz}})
                     </div>
 
-                    <div class="ui grid">
-                        <div class="four wide column">
-                            <mu-form-item prop="angle" label="Angle" class="range">
-                                <mu-text-field prop="angle" v-model="sensor.angleV"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="x" label="x" class="range">
-                                <mu-text-field prop="x"  v-model="sensor.vx"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="y" label="x" class="range">
-                                <mu-text-field prop="y"  v-model="sensor.vy"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="z" label="x" class="range">
-                                <mu-text-field prop="z"  v-model="sensor.vz"></mu-text-field>
-                            </mu-form-item>
-                        </div>
+                    <div class="eight wide column" style="text-align:center;">
+                        <img id="compass_v" ref="compass_v" src="/static/img/spotify_logo.png" style="width:100px;height:100px;border-radius:50%;"/><br/>
+                        Angle(v):{{sensor.angleV}}<br/>
+                        (x,y,z):({{sensor.vx}},{{sensor.vy}},{{sensor.vz}})
                     </div>
-
-                    <div class="ui grid">
-                        <div class="four wide column">
-                            <mu-form-item prop="absolute" label="Angle" class="range">
-                                <mu-text-field prop="absolute" v-model="sensor.absolute"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="alpha" label="alpha" class="range">
-                                <mu-text-field prop="alpha"  v-model="sensor.alpha"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="beta" label="beta" class="range">
-                                <mu-text-field prop="beta"  v-model="sensor.beta"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-
-                        <div class="four wide column">
-                            <mu-form-item prop="gamma" label="gamma" class="range">
-                                <mu-text-field prop="gamma"  v-model="sensor.gamma"></mu-text-field>
-                            </mu-form-item>
-                        </div>
-                    </div>
-                </mu-form>
-
-<!--                <div>-->
-<!--                    <div id="element_05_compass_h" style="margin:10px 0px 10px 100px; width:200px; height:200px; position:relative; line-height:1; background:#fcc; border:10px solid #caa; border-radius:100px;">-->
-<!--                        <div style="position:absolute; width:180px; height:180px; font-size:180px; font-weight:bold; top:0; bottom:0; left:0; right:0; margin:auto;">↑</div>-->
-<!--                    </div>-->
-<!--                    angle<br>-->
-<!--                    <input id="input_05_angle_h" style="margin-bottom:5px;"><br>-->
-<!--                    +(axisY)<br>-->
-<!--                    x:<input id="input_05_axis_y_x" style="width:70px;"> y:<input id="input_05_axis_y_y" style="width:70px;"> z:<input id="input_05_axis_y_z" style=" width:70px; margin-bottom:5px;"><br>-->
-<!--                    <hr>-->
-<!--                    デバイスを垂直に立てて閲覧している場合-->
-<!--                    <div id="element_05_compass_v" style="margin:-50px 0px -30px 100px; width:200px; height:200px; position:relative; line-height:1; background:#ccf; border:10px solid #aac;">-->
-<!--                        <div style="position:absolute; width:180px; height:180px; font-size:180px; font-weight:bold; top:0; bottom:0; left:0; right:0; margin:auto;">↑</div>-->
-<!--                    </div>-->
-<!--                    angle<br>-->
-<!--                    <input id="input_05_angle_v" style="margin-bottom:5px;"><br>-->
-<!--                    -(axisZ)<br>-->
-<!--                    x:<input id="input_05_axis_z_x" style="width:70px;"> y:<input id="input_05_axis_z_y" style="width:70px;"> z:<input id="input_05_axis_z_z" style=" width:70px; margin-bottom:5px;"><br>-->
-
-<!--                </div>-->
+                </div>
 
 
             </mu-flex>
@@ -198,7 +112,9 @@
                 let angleH = Math.atan2(-axisY.x,axisY.y) * (180.0 / Math.PI);
                 if(axisZ.z < 0) angleH = -angleH;
 
-                //element_compass_h.style.transform = "rotate(" + (angleH) + "deg)";
+                this.$ref.compass_h.style.transform = "rotate(" + (angleH) + "deg)";
+
+
                 this.sensor.angleH = angleH;
                 this.sensor.hx = (axisY.x).toFixed(4);
                 this.sensor.hy = (axisY.y).toFixed(4);
@@ -209,7 +125,7 @@
                 // ------------------------------------------------------------
                 let angleV = Math.atan2(axisZ.x,-axisZ.y) * (180.0 / Math.PI);
 
-                //element_compass_v.style.transform = "perspective(300px) rotateX(65deg) rotateZ(" + (angleV).toFixed(10) + "deg)";
+                this.$ref.compass_v.style.transform = "perspective(300px) rotateX(65deg) rotateZ(" + (angleV).toFixed(10) + "deg)";
                 this.sensor.angleV = angleV;
                 this.sensor.vx = (-axisZ.x).toFixed(4);
                 this.sensor.vy = (-axisZ.y).toFixed(4);
@@ -224,4 +140,12 @@
 </script>
 
 <style scoped lang="scss">
+
+    /*img#compass_h{*/
+    /*    transform: rotateX(165deg) rotateZ(10deg);*/
+    /*}*/
+
+    /*img#compass_v{*/
+    /*    transform: perspective(300px) rotateX(65deg) rotateZ(20deg);*/
+    /*}*/
 </style>
