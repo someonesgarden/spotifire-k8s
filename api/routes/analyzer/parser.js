dd04f350f070ea1803cb70c824d3659c8936e34c
Daisuke Nishimura <d@someonesgarden.org>
2019-07-03 21:17:28 +0900

    kuromoji dic fix

--- api/routes/analyzer/parser.js
+++ api/routes/analyzer/parser.js
@@ -4,15 +4,18 @@ let router = express.Router();
 
 
 let builder = kuromoji.builder({
-    dicPath: 'node_modules/kuromoji/dict'
+    //dicPath: 'node_modules/kuromoji/dict'
+    dicPath: 'public/dict'
 });
 
 router.post('/kuromoji', (req,res)=>{
     let q = req.body.q ? req.body.q : "今日は森へ行った";
+    console.log(q);
     builder.build(function(err, tokenizer) {
         if(err) {throw err;}
         let tokens = tokenizer.tokenize(q);
         res.send(tokens);
+        console.log(tokens);
     });
 });
 
